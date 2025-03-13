7fab8465e384bfe6947f28dbfc11231d2f5a329e
jazhou-adobe <31876837+jazhou-adobe@users.noreply.github.com>
2025-03-13 12:20:27 +1100

    Update teaser component to use kebab-case CSS selectors

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,11 +1,11 @@
 export default function decorate(block) {
-  // Add teaser__image class to image container
+  // Add teaser-image class to image container
   const imageDiv = block.querySelector('div > div:first-child');
   if (imageDiv) {
     imageDiv.classList.add('teaser-image');
   }
 
-  // Add teaser__content class and style content section
+  // Add teaser-content class and style content section
   const contentDiv = block.querySelector('div:nth-child(2)');
   if (contentDiv) {
     contentDiv.classList.add('teaser-content');
@@ -25,5 +25,18 @@ export default function decorate(block) {
     if (description) {
       description.classList.add('teaser-description');
     }
+    
+    // Move link element into content div
+    const linkDiv = block.querySelector('div:last-child div');
+    if (linkDiv) {
+      linkDiv.classList.add('teaser-link');
+      contentDiv.querySelector('div').appendChild(linkDiv);
+    }
+  }
+
+  // Remove the now empty last div (original link container)
+  const lastDiv = block.querySelector('div:last-child');
+  if (lastDiv && lastDiv.children.length === 0) {
+    lastDiv.remove();
   }
 }
